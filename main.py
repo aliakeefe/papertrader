3f3fb274e13643f4761b3ce2eb9ec5276284daf6
aliakeefe <aliakeefe@gmail.com>
2023-06-06 22:56:36 -0700

    second commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 # Importing the API and instantiating the REST client according to our keys
 
+#Importing the API and instantiating the REST client according to our keys
 from alpaca.trading.client import TradingClient
 
-API_KEY = "PKQI33ZN8PL4OFTAVDWM"
-SECRET_KEY = "jNa04HNvurPVwFKWGN0f0k9U7bZqfrCbIkkD5ODe"
+API_KEY = "PK756R9JT97GESCBUZIV"
+SECRET_KEY = "xfpnK4bk8oyzkOzxMqrEBcDavIFwZ1W8iYdMdCki"
 
 trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
-# Getting account information and printing it
+#Getting account information and printing it
 # account = trading_client.get_account()
 # for property_name, value in account:
-#   print(f"\"{property_name}\": {value}")
-
+#   print(f""{property_name}": {value}")
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
-# Setting parameters for our buy orde
+#Setting parameters for our buy orde
 market_order_data = MarketOrderRequest(
     symbol="TXG",
     qty=5,
@@ -23,25 +23,31 @@ market_order_data = MarketOrderRequest(
     time_in_force=TimeInForce.GTC
   )
 
-# Submitting the order and then printing the returned object
-market_order = trading_client.submit_order(market_order_data)
-for property_name, value in market_order:
-    print(f"\"{property_name}\": {value}")
-
-
-
-
+#Submitting the order and then printing the returned object
+#market_order = trading_client.submit_order(market_order_data)
+#for property_name, value in market_order:
+#print(f""{property_name}": {value}")
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest
 
-# keys required for stock historical data client
+#keys required for stock historical data client
 client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-# multi symbol request - single symbol is similar
-multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["SPY", "GLD", "TLT"])
+#multi symbol request - single symbol is similar
+multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["TXG"])
 
 latest_multisymbol_quotes = client.get_stock_latest_quote(multisymbol_request_params)
 
-gld_latest_ask_price = latest_multisymbol_quotes["GLD"].ask_price
+gld_latest_ask_price = latest_multisymbol_quotes["TXG"].ask_price
 
-print(latest_multisymbol_quotes.df)
+from alpaca.data.timeframe import TimeFrame
+from alpaca.data.requests import StockBarsRequest
+from datetime import datetime, timedelta
+request_params = StockBarsRequest(
+                        symbol_or_symbols=["TXG"],
+                        timeframe=TimeFrame.Day,
+                        start=datetime.now() - timedelta(days = 7),
+                        end=datetime.now()
+                        )
+data = client.get_stock_bars(request_params)
+print(data)
